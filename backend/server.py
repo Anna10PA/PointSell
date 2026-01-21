d080be044084c2fd53bd680f5e515a037e4b95ed
Anna10PA <puturidzeana0210@gmail.com>
2026-01-21 21:28:26 +0400

    commit

--- backend/server.py
+++ backend/server.py
@@ -51,9 +51,9 @@ my_password = os.environ.get('Gmail_password')
 # მეილზე გაგზავნის ფუნქცია
 app.config.update(
     MAIL_SERVER='smtp.gmail.com',
-    MAIL_PORT=587,
-    MAIL_USE_TLS=True,
-    MAIL_USE_SSL=False,
+    MAIL_PORT=465,
+    MAIL_USE_TLS=False,
+    MAIL_USE_SSL=True,
     MAIL_USERNAME='puturidzeana0210@gmail.com',
     MAIL_PASSWORD=os.environ.get('Gmail_password')
 )
