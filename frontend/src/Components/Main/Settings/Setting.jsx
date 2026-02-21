63796aa5f32523c3e17706b7fd72527f91ec2c3f
Anna10PA <puturidzeana0210@gmail.com>
2026-02-22 01:30:41 +0400

    commit

--- frontend/src/Components/Main/Settings/Setting.jsx
+++ frontend/src/Components/Main/Settings/Setting.jsx
@@ -79,7 +79,7 @@ function Setting() {
             </header>
             <section className='w-full flex items-start gap-5 max-lg:flex-col overflow-auto '>
                 <section className='border border-gray-300 rounded-lg h-full p-5 flex flex-col items-start gap-9 w-[40%] max-lg:w-full max-lg:h-screen max-sm:p-2'>
-                    <div className='flex items-center justify-center flex-col gap-3 border-b h-[33vh] border-gray-300 w-full'>
+                    <div className='flex items-center justify-center flex-col gap-3 border-b min-h-[33vh] h-full border-gray-300 w-full'>
                         <div className='w-35 h-35 relative'>
                             <img src={currentImage} className='w-full h-full object-cover rounded-[50%]' alt="" />
                             <div className='w-10 h-10 flex items-center justify-center bg-[#f67f20] rounded-[50%] text-white absolute top-2 -right-1 border-3 border-white cursor-pointer duration-100 hover:bg-orange-500'>
@@ -98,25 +98,27 @@ function Setting() {
                             <p className='font-semibold text-gray-400 max-sm:text-sm'>{curentUser?.position}</p>
                         </div>
                     </div>
-                    <div className='w-full flex flex-col items-start gap-4 max-sm:gap-3'>
-                        <Link to="/main/setting" className="w-full">
-                            <div className={`flex items-center justify-between border px-5 py-3 rounded cursor-pointer  hover:bg-gray-10 ${locationName == '/main/setting' ? 'border-[#f67f20] bg-[#f67f20] text-white' : 'border-gray-300'}`}>
-                                <h1 className='font-bold max-sm:text-[14px]'>Personal Information</h1>
-                                <i className="fa-solid fa-angle-right"></i>
-                            </div>
-                        </Link>
-                        <Link to="/main/setting/employers_information" className="w-full">
-                            <div className={`flex items-center justify-between border px-5 py-3 rounded cursor-pointer  hover:bg-gray-10 ${locationName == '/main/setting/employers_information' ? 'border-[#f67f20] bg-[#f67f20] text-white' : 'border-gray-300'} `}>
-                                <h1 className='font-bold max-sm:text-[14px]'>Employee Information</h1>
-                                <i className="fa-solid fa-angle-right"></i>
-                            </div>
-                        </Link>
-                        <Link to="/main/setting/password" className="w-full">
-                            <div className={`flex items-center justify-between border px-5 py-3 rounded cursor-pointer  hover:bg-gray-10 ${locationName === '/main/setting/password' || locationName === '/main/setting/password/verify' || locationName === '/main/setting/password/verify/reset_password' ? 'border-[#f67f20] bg-[#f67f20] text-white' : 'border-gray-300'}`}>
-                                <h1 className='font-bold max-sm:text-[14px]'>Password</h1>
-                                <i className="fa-solid fa-angle-right"></i>
-                            </div>
-                        </Link>
+                    <div className='w-full flex flex-col items-start justify-between gap-4 max-sm:gap-3'>
+                        <div className='w-full h-full flex flex-col items-start gap-5'>
+                            <Link to="/main/setting" className="w-full">
+                                <div className={`flex items-center justify-between border px-5 py-3 rounded cursor-pointer  hover:bg-gray-10 ${locationName == '/main/setting' ? 'border-[#f67f20] bg-[#f67f20] text-white' : 'border-gray-300'}`}>
+                                    <h1 className='font-bold max-sm:text-[14px]'>Personal Information</h1>
+                                    <i className="fa-solid fa-angle-right"></i>
+                                </div>
+                            </Link>
+                            <Link to="/main/setting/employers_information" className={`w-full ${curentUser?.position === 'Customer' ? 'hidden' : ''}`} >
+                                <div className={`flex items-center justify-between border px-5 py-3 rounded cursor-pointer  hover:bg-gray-10 ${locationName == '/main/setting/employers_information' ? 'border-[#f67f20] bg-[#f67f20] text-white' : 'border-gray-300'} `}>
+                                    <h1 className='font-bold max-sm:text-[14px]'>Employee Information</h1>
+                                    <i className="fa-solid fa-angle-right"></i>
+                                </div>
+                            </Link>
+                            <Link to="/main/setting/password" className="w-full">
+                                <div className={`flex items-center justify-between border px-5 py-3 rounded cursor-pointer  hover:bg-gray-10 ${locationName === '/main/setting/password' || locationName === '/main/setting/password/verify' || locationName === '/main/setting/password/verify/reset_password' ? 'border-[#f67f20] bg-[#f67f20] text-white' : 'border-gray-300'}`}>
+                                    <h1 className='font-bold max-sm:text-[14px]'>Password</h1>
+                                    <i className="fa-solid fa-angle-right"></i>
+                                </div>
+                            </Link>
+                        </div>
                         <button className='px-5 py-3 text-[#f67f20] font-bold text-lg duration-100 max-sm:text-[14px] hover:bg-[#f67f20] hover:text-white cursor-pointer rounded' onClick={log_out}>Log out</button>
                     </div>
                 </section>
