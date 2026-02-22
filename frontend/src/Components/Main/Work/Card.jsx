a67117d3013d7e7811bcb775096543d3f6729f60
Anna10PA <puturidzeana0210@gmail.com>
2026-02-22 13:25:54 +0400

    commit

--- frontend/src/Components/Main/Work/Card.jsx
+++ frontend/src/Components/Main/Work/Card.jsx
@@ -5,31 +5,27 @@ function Card({ user, text, time }) {
   let { answer, curentUser } = useContext(Info)
 
   return (
-    <div className='bg-gray-100 px-7 py-5 rounded-2xl w-full flex flex-col items-start gap-5 border border-gray-200'>
+    <div className='bg-gray-100 px-7 py-5 max-md:px-4 max-md:py-3 max-md:text-sm rounded-2xl w-full flex flex-col items-start gap-5 border border-gray-200 relative'>
       <div className='flex items-center justify-between w-full'>
-        <h1 className='text-gray-500 text-xl font-bold'>{user}</h1>
-        <p className='text-gray-500 font-semibold'>{(String(new Date(time.split(' ')[0])).split(' ').slice(1, 4)).join(' ')}</p>
+        <h1 className='text-gray-500 text-xl font-bold max-md:text-[17px] break-all'>{user}</h1>
+        <p className='text-gray-500 font-semibold max-md:text-sm max-md:absolute max-md:bottom-3 max-md:right-5'>{(String(new Date(time.split(' ')[0])).split(' ').slice(1, 4)).join(' ')}</p>
       </div>
       <p className='text-gray-400 font-semibold'>{text}</p>
-      <div className='w-full flex items-center justify-between'>
-        <div className={`flex items-center gap-5 ${curentUser?.position == 'Manager' ? ' ': 'hidden'}`}>
-          <button className='px-5 py-2 rounded bg-[#f67f20] flex items-center gap-3 justify-center text-white font-bold tracking-wide duration-100 hover:bg-orange-400 cursor-pointer' onClick={()=> {
+      <div className='w-full flex items-center justify-between '>
+        <div className={`flex items-center gap-5 ${curentUser?.position == 'Manager' ? ' ': 'hidden'} max-md:flex-col max-md:items-start max-md:gap-2`}>
+          <button className='px-5 py-2 rounded bg-[#f67f20] flex items-center gap-3 justify-center text-white font-bold tracking-wide duration-100 hover:bg-orange-400 cursor-pointer max-md:px-3 max-md:py-1.5 max-md:text-sm' onClick={()=> {
             answer(true, user)
           }}>
             <i className="fa-solid fa-check"></i>
             Accept
           </button>
-          <button className='px-5 py-2 rounded bg-[#f62020] flex items-center gap-3 justify-center text-white font-bold tracking-wide duration-100 hover:bg-red-500 cursor-pointer' onClick={()=> {
+          <button className='px-5 py-2 rounded bg-[#f62020] flex items-center gap-3 justify-center text-white font-bold tracking-wide duration-100 hover:bg-red-500 cursor-pointer max-md:px-3 max-md:py-1.5 max-md:text-sm' onClick={()=> {
             answer(false, user)
           }}>
             <i className="fa-solid fa-xmark"></i>
             Reject
           </button>
         </div>
-        <button className='px-5 py-2 rounded bg-gray-500 flex items-center gap-3 justify-center text-white font-bold tracking-wide duration-100 hover:bg-gray-400 cursor-pointer'>
-          <i className="fa-solid fa-address-card"></i>
-          Info
-        </button>
       </div>
     </div>
   )
